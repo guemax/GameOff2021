228e387539e60029fe060f90d62367debe3766d1
Max <github-mg@guenther.cloud>
2021-11-21 18:31:50 +0100

    Remove fade from credits button
    Fade animation when switching scenes should only be visible when switching to the evaluate scene

--- src/godot/scripts/button-credits.gd
+++ src/godot/scripts/button-credits.gd
@@ -1,6 +1,4 @@
 extends Button
 
 func _on_ButtonCredits_pressed():
-	TransitionScreen.transition()
-	yield(get_tree().create_timer(1), "timeout")
 	get_tree().change_scene("res://scenes/credits.tscn")
