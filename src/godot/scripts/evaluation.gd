c9174df8005d7c6f44b4ef25534371c43c39f5ff
Max <github-mg@guenther.cloud>
2021-11-21 17:36:19 +0100

    Refine evaluation.gd
    Add comments and modify some code to make it 'cleaner'

--- src/godot/scripts/evaluation.gd
+++ src/godot/scripts/evaluation.gd
@@ -4,54 +4,52 @@ var collected_passwords = 0
 var all_passwords = 0
 
 var time = 0
-var best_time = 105 # equals "01:45", tried it out
+var best_time = 250 # equals "04:17", enough time to collect passwords
 
 var points = 0
 
 func _ready():
+	# Read data from global script
 	collected_passwords = global.collected_passwords
 	all_passwords = global.all_passwords
 	time = global.time
-#
-#	collected_passwords = 5
-#	time = 75
+
+	# For debugging purposes
+#	collected_passwords = 10
+#	time = 200
 	
+	# Set max number of passwords
 	$passwords/title/value.text = "0 of " + String(all_passwords)
 	
 	yield(get_tree().create_timer(1.0), "timeout")
 	count_passwords_up(0)
 	count_time_up(0)
 	
-	yield(get_tree().create_timer(1.5), "timeout")
+	yield(get_tree().create_timer(time * 0.02 + 0.5), "timeout")
 
 	evaluate()
-	count_points_up(0)
+	$final/title/value.text = String(points) + " Points"
 	
 func evaluate():
-	points = collected_passwords * 50
-	print(points)
-	
-	var points_for_time = (best_time / time) * 1000
-	print(points_for_time)
-	
-	points += points_for_time
-	print(points)
-
-func count_points_up(counted_points):
-	while counted_points < points:
-		counted_points += 1
-		$final/title/value.text = String(counted_points) + " Points"
-		count_passwords_up(counted_points)
+	# Compute the final points gained
+	points = collected_passwords * 100
+	if time == 0:
+		pass
+	else:
+		points += (best_time * 1.25 / time) * 1000
+		points = round(points)	# I don't want the points to have a decimal point
 
 func count_passwords_up(counted_passwords):
-	while counted_passwords < collected_passwords:
+	# Count the collected passwords up using recursion
+	if counted_passwords < collected_passwords:
 		counted_passwords += 1
 		$passwords/title/value.text = String(counted_passwords) + " of " + String(all_passwords)
 		yield(get_tree().create_timer(0.05), "timeout")
 		count_passwords_up(counted_passwords)
 
 func count_time_up(counted_time):
-	while counted_time < time:
+	# Count the time up using recursion
+	if counted_time < time:
 		counted_time += 1
 		
 		var seconds = fmod(counted_time, 60)
@@ -61,4 +59,4 @@ func count_time_up(counted_time):
 	
 		$time/title/value.text = time_passed
 		yield(get_tree().create_timer(0.01), "timeout")
-		count_passwords_up(counted_time)
+		count_time_up(counted_time)
