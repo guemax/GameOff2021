439d01d100770bcab4f2d457b2926bc1b7114589
Max <github-mg@guenther.cloud>
2021-11-21 12:24:03 +0100

    Modify the menu and add credits
    Add a quit game button and  a menu for the credits

---
+++ src/godot/scripts/button-back.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_Buttonback_pressed():
+	get_tree().change_scene("res://scenes/menu.tscn")
---
+++ src/godot/scripts/button-credits.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_ButtonCredits_pressed():
+	get_tree().change_scene("res://scenes/credits.tscn")
