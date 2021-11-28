33e55d0a2ef8a08c50a280c25183cad34626926f
Max <github-mg@guenther.cloud>
2021-11-28 18:57:46 +0100

    Display number of collectable passwords
    Display the passwords so a player knows how many passwords are collectable (example: 7/20)

--- src/godot/scripts/CounterPasswords.gd
+++ src/godot/scripts/CounterPasswords.gd
@@ -1,11 +1,16 @@
 extends Label
 
 var passwords = 0
+var all_passwords = 0
+
+func set_text_passwords():
+	text = String(passwords) + "/" + String(all_passwords)
 
 func _ready():
-	text = String(passwords)
+	all_passwords = global.all_passwords
+	set_text_passwords()
 
 func _on_password_collected():
 	passwords += 1
-	text = String(passwords)
+	set_text_passwords()
 	global.collected_passwords += 1
