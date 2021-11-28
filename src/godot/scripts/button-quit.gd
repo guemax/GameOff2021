ec9f016bceefe59324d0c0193de365809fbc4dc6
Max <github-mg@guenther.cloud>
2021-11-28 18:50:34 +0100

    Remove unnecessary comments
    Clean the script files by removing unnecessary comments and unused functions

--- src/godot/scripts/button-quit.gd
+++ src/godot/scripts/button-quit.gd
@@ -1,6 +1,4 @@
 extends Button
 
-
-
 func _on_ButtonQuit_pressed():
 	get_tree().quit()
--- src/godot/scripts/global_values.gd
+++ src/godot/scripts/global_values.gd
@@ -4,6 +4,3 @@ var collected_passwords = 0
 var all_passwords = 20
 
 var time = 0
-#
-#func _process(delta):
-#	print(collected_passwords)
