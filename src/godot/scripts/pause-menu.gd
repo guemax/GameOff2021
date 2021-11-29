e8206c8d8be162dbe33548c2ac4d0ea6cdccdc3c
Max <github-mg@guenther.cloud>
2021-11-29 19:26:15 +0100

    Make pause menu close on keypress
    Make the pause menu closebable when pressing Esc the second time

--- src/godot/scripts/pause-menu.gd
+++ src/godot/scripts/pause-menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var pause_menu_opened = false
+
 func show_paus_menu():
 	$AnimationPlayer.play("fade-in")
 
@@ -7,5 +9,9 @@ func _on_ButtonClose_pressed():
 	$AnimationPlayer.play("fade-out")
 
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") and not pause_menu_opened:
 		show_paus_menu()
+		pause_menu_opened = true
+	elif event.is_action_pressed("ui_cancel") and pause_menu_opened:
+		$AnimationPlayer.play("fade-out")
+		pause_menu_opened = false
