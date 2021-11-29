3b797875b9de82aa3ddd750140b30063557c6bf0
Max <github-mg@guenther.cloud>
2021-11-29 19:29:07 +0100

    Fix bug concerning pause menu
    Make the script change  not only when pressing Esc, but also when using the button

--- src/godot/scripts/pause-menu.gd
+++ src/godot/scripts/pause-menu.gd
@@ -4,14 +4,16 @@ var pause_menu_opened = false
 
 func show_paus_menu():
 	$AnimationPlayer.play("fade-in")
+	pause_menu_opened = true
 
 func _on_ButtonClose_pressed():
 	$AnimationPlayer.play("fade-out")
+	pause_menu_opened = false
+	
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel") and not pause_menu_opened:
 		show_paus_menu()
-		pause_menu_opened = true
 	elif event.is_action_pressed("ui_cancel") and pause_menu_opened:
 		$AnimationPlayer.play("fade-out")
 		pause_menu_opened = false
