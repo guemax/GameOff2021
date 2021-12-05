fc6cdb4aadb8300a8f58cc87a6a76b479e52c7fb
Max <github-mg@guenther.cloud>
2021-12-05 11:38:23 +0100

    Refactor logic in CounterPasswords.gd
    Refactor the functions in this file to make it shorter and easier to read.

--- src/godot/scripts/CounterPasswords.gd
+++ src/godot/scripts/CounterPasswords.gd
@@ -1,16 +1,11 @@
 extends Label
 
-var passwords = 0
-var all_passwords = 0
-
-func set_text_passwords():
-	text = String(passwords) + "/" + String(all_passwords)
-
 func _ready():
-	all_passwords = global.all_passwords
 	set_text_passwords()
 
 func _on_password_collected():
-	passwords += 1
+	global.collected_passwords += 1	
 	set_text_passwords()
-	global.collected_passwords += 1
+
+func set_text_passwords():
+	text = String(global.collected_passwords) + "/" + String(global.all_passwords)
