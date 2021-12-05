b5fb85fc8b01bfd7b322f118c8431a5a901d4411
Max <github-mg@guenther.cloud>
2021-12-05 13:56:10 +0100

    Remove script controlling the main menu music
    The music is autplaying, so no need for letting it start via a script and modify the stop function to be able to delete this script completely.

--- src/godot/scripts/button-play.gd
+++ src/godot/scripts/button-play.gd
@@ -3,6 +3,5 @@ extends Button
 func _on_buttonplay_pressed():
 	get_tree().change_scene("res://scenes/level1.tscn")
 
-
 func _on_ButtonPlay_pressed_stop_music():
-	main_menu_music.stop_music_from_playing()
+	main_menu_music.stop()
--- src/godot/scripts/main-menu-music-player.gd
+++
@@ -1,7 +0,0 @@
-extends AudioStreamPlayer
-
-func _ready():
-	play()
-
-func stop_music_from_playing():
-	stop()
