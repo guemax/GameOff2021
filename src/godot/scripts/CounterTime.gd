18416026b4c437a7b7734c1e53ea8842caca23ec
Max <github-mg@guenther.cloud>
2021-12-05 11:43:40 +0100

    Refactor logic in CounterTime.gd
    Refactor the functions in this file to make it shorter and easier to understand

--- src/godot/scripts/CounterTime.gd
+++ src/godot/scripts/CounterTime.gd
@@ -1,21 +1,19 @@
 extends Label
 
-var time = 0
-
 func _ready():
 	global.timer_on = true
 
 func _process(delta):
 	if (global.timer_on):
-		time += delta
+		global.time += delta
 	
-	var seconds = fmod(time, 60)
-	var mins = fmod(time, 60*60) / 60
+	var seconds = fmod(global.time, 60)
+	var mins = fmod(global.time, 60*60) / 60
 	
+	# Format is 00:00
 	var time_passed = "%02d:%02d" % [mins, seconds]
 	text = time_passed
 
 func _on_Area_body_entered(body):
 	if body.name == "Bug":
 		global.timer_on = false
-		global.time = time
